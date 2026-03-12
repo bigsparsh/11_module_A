83fd6353c7adb6b9537344e1c8abe803cff360fc
Sparsh Singh <sparshsingh7586@gmail.com>
2026-03-12 14:28:18 +0530

    Added index

--- app/11_module_A/page.tsx
+++ app/11_module_A/page.tsx
@@ -10,33 +10,33 @@ import a6_mock from "../../public/a6_mock.png";
 const Index = () => {
 
     return <div className="grid grid-cols-3 px-10 m-0 w-screen *:text-white bg-emerald-900" >
-        <Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
+        <Link href={"/11_module_A/A1"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
 <img src={a1_mock.src} />
 <h1 className="font-bold">Module A1
 </h1>
         </Link>
-        <Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
+        <Link href={"/11_module_A/A2"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
 <img src={a2_mock.src} />
 <h1 className="font-bold">Module A2
 </h1>
         </Link>
-        <Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
+        <Link href={"/11_module_A/A3"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
 <img src={a3_mock.src} />
 <h1 className="font-bold">Module A3
 </h1>
         </Link>
-        <Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
+        <Link href={"/11_module_A/A4"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
 <img src={a4_mock.src} />
 <h1 className="font-bold">Module A4
 </h1>
         </Link>
-        <Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100  rounded-xl">
+        <Link href={"/11_module_A/A5"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100  rounded-xl">
 <img src={a5_mock.src} />
 <h1 className="font-bold">Module A5
 </h1>
         </Link>
 
-<Link href={""} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
+<Link href={"/11_module_A/A6"} className="flex flex-col w-[300px] h-[550px] justify-center items-center gap-3 border border-emerald-100 rounded-xl">
 <img src={a6_mock.src} />
 <h1 className="font-bold">Module A6
 </h1>
